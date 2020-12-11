ccce696a1781315e0e360a93d0fe0d372a22837a
Yonarp <pranoy0211@gmail.com>
2020-12-12 02:36:49 +0530

    added google API for search algo amd usegooglesearch hook

---
+++ src/API/keys.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'AIzaSyBQTnfW2cCxGTNrbKlYbYHJsKdC2n8bJVw';
+
+export const CONTEXT_KEY = 'b6c321eb0a6c4d0c1';
+
--- src/Pages/SearchPage/searchpage.component.jsx
+++ src/Pages/SearchPage/searchpage.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Searchbox} from '../../Components/searchbox/searchbox.component';
 import {useStateValue} from '../../State/stateprovider.component';
+import useGoogleSearch from '../../Components/Hooks/useGoogleSearch.component';
 
 const SearchPage = () => {
 const [state] = useStateValue();
+const {data} = useGoogleSearch(state.term);
+console.log(data);
  return(   <div className="">
         <h1>This is the search Page about {state.term} </h1>
         <Searchbox/>
